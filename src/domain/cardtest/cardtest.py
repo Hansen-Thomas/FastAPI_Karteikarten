09bb6a4f4c7f58bc2bd4680f48aa0efad4bbd208
Thomas Hansen <thomas.hansen@zeitgeist.watch>
2024-12-20 18:17:32 +0100

    Minor adjustments

--- src/domain/cardtest/cardtest.py
+++ src/domain/cardtest/cardtest.py
@@ -4,13 +4,13 @@ from domain.cardset.cardset import Cardset
 
 class Cardtest:
     
-    def __init__(self, name: str, cardset: Cardset):
-        self.name = name
-        self.cardset = cardset
-        self.current_card_index = 0
-        self.correct_answers = 0
-        self.wrong_answers = 0
-        self.last_answer_correct = False
+    def __init__(self, name: str, cardset: Cardset) -> None:
+        self.name: str = name
+        self.cardset: Cardset = cardset
+        self.current_card_index: int = 0
+        self.correct_answers: int = 0
+        self.wrong_answers: int = 0
+        self.last_answer_correct: bool = False
         
     def __str__(self):
         return f"{self.name} ({self.correct_answers} correct, {self.wrong_answers} wrong)"
