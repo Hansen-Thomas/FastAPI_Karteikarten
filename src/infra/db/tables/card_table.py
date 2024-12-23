f30724395feb61eb4736e725d79aa080877281e9
Thomas Hansen <thomas.hansen@zeitgeist.watch>
2024-12-23 15:30:01 +0100

    Map more card-fields to db

--- src/infra/db/tables/card_table.py
+++ src/infra/db/tables/card_table.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, Column, Integer, String, Enum
+from sqlalchemy import Table, Column, Boolean, Integer, String, Enum, DateTime
 
 from domain.card.word_type import WordType
 from infra.db import metadata
@@ -11,4 +11,8 @@ card_table = Table(
     Column("word_type", Enum(WordType)),
     Column("german", String),
     Column("italian", String),
+    Column("times_played", Integer),
+    Column("correct_answers", Integer),
+    Column("last_answer_correct", Boolean),
+    Column("last_played", DateTime),
 )
